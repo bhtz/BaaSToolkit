d3f31a63ba3dfc90b52858e58e727511a414fb79
Billy Courteaud <billy.courteaud@gmail.com>
2023-03-24 19:23:12 +0100

    feat: add nuxt3 + tailwind + first example page

---
+++ src/clients/nuxt/pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+<section class="bg-[url('https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/coast-house-view.jpg')] bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply ">
+<div class="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
+  <div class="mb-6 max-w-screen-lg lg:mb-0">
+    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+    <p class="mb-6 font-light text-gray-400 lg:mb-8 md:text-lg lg:text-xl">The need for energy is universal. That's why Flowbite scientists and engineers are pioneering new research and pursuing new technologies to reduce emissions while creating more efficient fuels. We're committed to responsibly meeting the world's energy needs.</p>
+    <a href="#" class="inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+      Learn more about the plan
+      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+  </div>
+  <div class="grid gap-8 pt-8 lg:pt-12 mt-8 lg:mt-12 border-t border-gray-600 sm:grid-cols-2 lg:grid-cols-4">
+    <div>
+      <h2 class="mb-1 text-lg font-bold">Fourth quarter 2021</h2>
+      <p class="mb-1 text-sm text-gray-400">We announced fourth quarter 2021 results</p>
+      <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+        Read more
+        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </a>
+    </div>
+    <div>
+      <h2 class="mb-1 text-lg font-bold">Zero emissions</h2>
+      <p class="mb-1 text-sm text-gray-400">Flowbite aims to achieve net-zero emissions</p>
+      <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+        Read more
+        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </a>
+    </div>
+    <div>
+      <h2 class="mb-1 text-lg font-bold">New York</h2>
+      <p class="mb-1 text-sm text-gray-400">Plans for net zero emissions in New York </p>
+      <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+        Read more
+        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </a>
+    </div>
+    <div>
+      <h2 class="mb-1 text-lg font-bold">2022 plans</h2>
+      <p class="mb-1 text-sm text-gray-400">Investing in the future of Africa</p>
+      <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+        Read more
+        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </a>
+    </div>
+  </div>
+</div>
+</section>
+</template>
